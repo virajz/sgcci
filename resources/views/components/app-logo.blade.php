102db557a79b49ffddef60ac94c1f514dffa84a8
Viraj Zaveri <44199181+virajz@users.noreply.github.com>
2026-03-10 08:59:01 +0530

    App Icon

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,4 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
-</div>
+<x-app-logo-icon class="size-8 fill-current text-[#102a83] dark:text-white" />
 <div class="ms-1 grid flex-1 text-start text-sm">
     <span class="mb-0.5 truncate leading-tight font-semibold">SGCCI</span>
 </div>
